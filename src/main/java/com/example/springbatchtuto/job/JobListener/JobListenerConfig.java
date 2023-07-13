4badc49d874587f9538093329cdd1b6c48f17e54
unknown <2hojune@naver.com>
2023-07-13 23:29:21 +0900

    ✨ Feat: Listener 방식 추가

---
+++ src/main/java/com/example/springbatchtuto/job/JobListener/JobListenerConfig.java
@@ -0,0 +1,62 @@
+package com.example.springbatchtuto.job.JobListener;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+/**
+ * desc: Hello World를 출력
+ * run: --spring.batch.job.names=jobListenerJob
+ * */
+@Configuration
+@RequiredArgsConstructor
+public class JobListenerConfig {
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job jobListenerJob(Step jobListenerStep) {
+        return jobBuilderFactory.get("jobListenerJob")
+                                .incrementer(new RunIdIncrementer())
+                                .listener(new jobLoggerListener())
+                                .start(jobListenerStep)
+                                .build();
+    }
+
+    @JobScope
+    @Bean
+    public Step jobListenerStep(Tasklet jobListenerTasklet) {
+        return stepBuilderFactory.get("jobListenerStep")
+                                 .tasklet(jobListenerTasklet)
+                                 .build();
+    }
+
+    @StepScope
+    @Bean
+    public Tasklet jobListenerTasklet() {
+        return new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+//                System.out.println("job Listener Tasklet");
+//                return RepeatStatus.FINISHED;
+
+                // 오류 발생 시 jobLoggerListener -> afterJob(BatchStatus.FAILED) 실행
+                throw new Exception("Failed!!!!");
+            }
+        };
+    }
+}
