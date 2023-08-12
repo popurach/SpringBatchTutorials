b069d0e7084f1f61ef0ab77fe3d738b19cae1bb4
unknown <2hojune@naver.com>
2023-08-12 23:49:45 +0900

    ✨ Feat: findByLandmarkAndMember 쿼리추가

--- src/main/java/com/example/springbatchtuto/core/domain/auction/repository/AuctionLogCustomQueryRepository.java
+++ src/main/java/com/example/springbatchtuto/core/domain/auction/repository/AuctionLogCustomQueryRepository.java
@@ -17,6 +17,28 @@ public class AuctionLogCustomQueryRepository {
 
     private final JPAQueryFactory queryFactory;
 
+    /**
+     * @param landmarkId
+     * @param memberId
+     * fetchOne : 단건을 조회할 때 사용하는 방법, 결과가 없을때는 null 반환, 둘 이상일 떄는 NonUniqueResultException
+     * fetchFirst : 처음의 한 건을 쿼리에서 가져오고 싶을 떄
+     * */
+    public Optional<AuctionLog> findByLandmarkAndMember(Long memberId, Long landmarkId) {
+        return Optional.ofNullable(
+                queryFactory.selectFrom(auctionLog)
+                        .join(auctionLog.auction, auction)
+                        .where(
+                                auction.finished.isFalse(),
+                                auctionLog.member.id.eq(memberId),
+                                auction.landmark.id.eq(landmarkId)
+                        )
+                        .fetchOne()
+        );
+    }
+
+    /**
+     * @param landmarkId 해당 랜드마크에 등록된 가장 첫번째 입찰 기록 리턴
+     * */
     public Optional<AuctionLog> findFirstByLandmarkId(Long landmarkId) {
         return Optional.ofNullable(
                 queryFactory.selectFrom(auctionLog)
